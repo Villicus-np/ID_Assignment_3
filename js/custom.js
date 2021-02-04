029d54c44bf680ed2dbec6e81264352438400f2a
Eddiechang99 <s10207896@connect.np.edu.sg>
2021-02-04 21:46:00 +0800

    navbar background change colour on scroll

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,6 @@
-/*!
- * Item: Kitzu
- * Description: Personal Portfolio Template
- * Author/Developer: Exill
- * Author/Developer URL: https://themeforest.net/user/exill
- * Version: v1.1.0
- * License: Themeforest Standard Licenses: https://themeforest.net/licenses
- */
-
-/*----------- CUSTOM JS SCRIPTS -----------*/
-
-(function($) {
-  'use strict';
-  $(function() {
-    // Code here executes When the DOM is loaded...
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $("#navbar");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
-  $(window).on('load', function() {
-    // Code here executes When the page is loaded
-  });
-}(jQuery));
+});
