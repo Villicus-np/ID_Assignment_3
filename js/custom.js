9c324674630818bbf6a6a6ee17b3a41896188ff9
Eddiechang99 <s10207896@connect.np.edu.sg>
2021-02-04 21:59:05 +0800

    added an on load force scroll to top in JS

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,13 @@
+// navbar background colour change on scroll
+
 $(function () {
   $(document).scroll(function () {
     var $nav = $("#navbar");
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
 });
+
+// arrow down icon onclick scroll to tabs
 
 $('#scroll-icon').mousedown(function(){
   timeout = setInterval(function(){
@@ -17,3 +21,9 @@ $(document).mouseup(function(){
   clearInterval(timeout);
   return false;
 });
+
+// force site to scroll to top on load
+
+$(document).ready(function() {
+  $(this).scrollTop(0);
+}
