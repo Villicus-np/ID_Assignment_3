c0d56441575c46be3380e8937aa07513b2f17aa2
Villicus-np <s10207947@connect.np.edu.sg>
2021-02-09 13:35:19 +0800

    Added in Js functions for API and graph

--- js/custom.js
+++ js/custom.js
@@ -26,4 +26,208 @@ $(document).mouseup(function(){
 
 $(document).ready(function() {
   $(this).scrollTop(0);
+
+  // API call
+  let url = 'https://global-warming.org/api/';
+
+  //temp, carbon, methane, nitruous, ice
+  apiCall("temp");
 })
+
+function loadChart(api_array,array_Type)
+{
+  // Google Charts https://developers.google.com/chart/interactive/docs/gallery/linechart
+  google.charts.load('current', {packages: ['corechart', 'line']});
+  google.charts.setOnLoadCallback(function () {
+    drawChart(api_array,array_Type);
+  });
+}
+
+function apiCall(apiLink)
+{
+  if(apiLink == "temp")
+  {
+    url = url + "temperature-api";
+  }
+  else if (apiLink == "carbon")
+  {
+    url = url + "co2-api";
+  }
+  else if (apiLink == "methane")
+  {
+    url = url + "methane-api";
+  }
+  else if (apiLink == "nitrous")
+  {
+    url = url + "nitrous-oxide-api";
+  }
+  else if (apiLink == "ice")
+  {
+    url = url + "arctic-api";
+  }
+
+  // Headers result[i] -> land,station,time
+  fetch(url)
+    .then(response => response.json()) 
+    .then(apiData)
+    .catch(err => {
+      console.error(err);
+    });
+    console.log();
+}
+
+function apiData(data) 
+{
+
+  var data_array = [];
+
+  if (url == 'https://global-warming.org/api/temperature-api')
+  {
+    // Headers: time,station
+    console.log("temp");
+    let result = data.result;
+
+    for (var i = 0; i<result.length;i++)
+    {
+      var objarray = [Number(result[i].time),Number(result[i].station)];
+      data_array.push(objarray);
+    }
+    loadChart(data_array,"temp");
+  }
+  else if (url == 'https://global-warming.org/api/co2-api')
+  {
+    // Headers: year,month,day,trend
+    console.log("co2");
+    let result = data.co2;
+
+    for (var i = 0; i<result.length;i++)
+    {
+      var objarray = [i,Number(result[i].trend)];
+      data_array.push(objarray);
+    }
+    loadChart(data_array,"carbon");
+  }
+  else if (url == 'https://global-warming.org/api/methane-api')
+  {
+    // Headers: date,average
+    console.log("methane");
+    let result = data.methane;
+
+    for (var i = 0; i<result.length;i++)
+    {
+      var objarray = [i,Number(result[i].average)];
+      data_array.push(objarray);
+    }
+    loadChart(data_array,"methane");
+  }
+  else if (url == 'https://global-warming.org/api/nitrous-oxide-api')
+  {
+    // Headers: date,average
+    console.log("nitrous");
+    let result = data.nitrous;
+
+    for (var i = 0; i<result.length;i++)
+    {
+      var objarray = [i,Number(result[i].average)];
+      data_array.push(objarray);
+    }
+    loadChart(data_array,"nitrous");
+  }
+  else if (url == 'https://global-warming.org/api/arctic-api')
+  {
+    // Headers: year,extend,area
+    console.log("artic");
+    let result = data.result;
+
+    for (var i = 0; i<result.length;i++)
+    {
+      var objarray = [i,Number(result[i].extend),Number(result[i].area)];
+      data_array.push(objarray);
+    }
+    loadChart(data_array,"ice");
+  }
+}
+
+
+function drawChart(dataArr,array_Type) 
+{
+  var data = new google.visualization.DataTable();
+  console.log(array_Type);
+  if(array_Type == "temp")
+  {
+    data.addColumn('number', 'X');
+    data.addColumn('number', 'Temperature');
+    
+    var options = {
+      hAxis: {
+        title: 'Year'
+      },
+      vAxis: {
+        title: 'Celsius'
+      }
+    };
+  }
+  else if (array_Type == "carbon")
+  {
+    data.addColumn('number', 'X');
+    data.addColumn('number', 'Carbon Dioxide');
+    
+    var options = {
+      hAxis: {
+        title: 'Year'
+      },
+      vAxis: {
+        title: 'Part Per million(ppm)'
+      }
+    };
+  }
+  else if (array_Type == "methane")
+  {
+    data.addColumn('number', 'X');
+    data.addColumn('number', 'Methane');
+    
+    var options = {
+      hAxis: {
+        title: 'Year'
+      },
+      vAxis: {
+        title: 'CO2 Levels'
+      }
+    };
+  }
+  else if (array_Type == "nitrous")
+  {
+    data.addColumn('number', 'X');
+    data.addColumn('number', 'Nitrous Oxide');
+    
+    var options = {
+      hAxis: {
+        title: 'Year'
+      },
+      vAxis: {
+        title: 'Nitrous Oxide mole fraction(ppb)'
+      }
+    };
+  }
+  else if (array_Type == "ice")
+  {
+    data.addColumn('number', 'X');
+    data.addColumn('number', 'Extend');
+    data.addColumn('number', 'Area');
+    
+    var options = {
+      hAxis: {
+        title: 'Year'
+      },
+      vAxis: {
+        title: 'Million Square km'
+      }
+    };
+  }
+  
+  data.addRows(dataArr);
+  
+  var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+  
+  chart.draw(data, options);
+}
