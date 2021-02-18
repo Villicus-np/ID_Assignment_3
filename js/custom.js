239ed3c1c86cb25beba43590710c72bd213eddb9
Villicus-np <s10207947@connect.np.edu.sg>
2021-02-18 23:36:45 +0800

    Added in Quiz codes

--- js/custom.js
+++ js/custom.js
@@ -85,56 +85,44 @@ function apiData(data) {
       var t = result[i].time;
       var year = t.substr(0, t.lastIndexOf("."));
       var month = t.substring(t.indexOf(".") + 1);
-      if (month == "04")
-      {
+      if (month == "04") {
         month = 1;
       }
-      else if (month == "13")
-      {
+      else if (month == "13") {
         month = 2;
       }
-      else if (month == "21")
-      {
+      else if (month == "21") {
         month = 3;
       }
-      else if (month == "29")
-      {
+      else if (month == "29") {
         month = 4;
       }
-      else if (month == "38")
-      {
+      else if (month == "38") {
         month = 5;
       }
-      else if (month == "46")
-      {
+      else if (month == "46") {
         month = 6;
       }
-      else if (month == "54")
-      {
+      else if (month == "54") {
         month = 7;
       }
-      else if (month == "63")
-      {
+      else if (month == "63") {
         month = 8;
       }
-      else if (month == "71")
-      {
+      else if (month == "71") {
         month = 9;
       }
-      else if (month == "79")
-      {
+      else if (month == "79") {
         month = 10;
       }
-      else if (month == "88")
-      {
+      else if (month == "88") {
         month = 11;
       }
-      else if (month == "96")
-      {
+      else if (month == "96") {
         month = 12;
       }
-      
-      var objarray = [new Date(year,month), Number(result[i].station)];
+
+      var objarray = [new Date(year, month), Number(result[i].station)];
       data_array.push(objarray);
     }
     console.log(data_array)
@@ -153,7 +141,7 @@ function apiData(data) {
       var objarray = [new Date(years, month, date), Number(result[i].trend)];
       data_array.push(objarray);
     }
-    
+
     loadChart(data_array, "carbon");
   }
   else if (url == 'https://global-warming.org/api/methane-api') {
@@ -179,7 +167,7 @@ function apiData(data) {
 
     console.log(result);
     for (var i = 0; i < result.length; i++) {
-      var objarray = [new Date(result[i].year,0), result[i].extent, result[i].area];
+      var objarray = [new Date(result[i].year, 0), result[i].extent, result[i].area];
       data_array.push(objarray);
     }
     loadChart(data_array, "ice");
@@ -293,96 +281,387 @@ apiCall("temp");
 
 //-------- Quiz JS Code -------------//
 
-var qnsArray = ["What makes the world hotter", "For what reason do i live here", "Which one is correct?", "What do you suffer for."];
-var quizDiv = document.getElementById('quiz_qns');
-for (var i = 0; i < qnsArray.length; i++) {
-  // quizDiv.append(qnsArray[i]);
+
+// Creating questionss and answers
+//*****************************************************************************
+var question1 = {
+  question: "What does HTML stand for?",
+  answers: ["Hyper Text Markup Language", "HyperLinks and Text Markup Language", "Home Tool Markup Language"],
+  correct: 0
+};
+
+var question2 = {
+  question: "The Bootstrap grid system is based on how many columns?",
+  answers: ['6', '9', '12', '3'],
+  correct: 2
+};
+
+var question3 = {
+  question: 'What does CSS stand for?',
+  answers: ['Colorful Style Sheets', 'Cascading Style Sheets', 'Creative Style Sheets', 'Computer Style Sheets'],
+  correct: 1
 }
 
-// pos is position of where the user in the test or which question they're up to
-var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, correct = 0;
-// this is a multidimensional array with 4 inner array elements with 5 elements inside them
-var questions = [
-  {
-    question: "What is 36 + 42",
-    a: "64",
-    b: "78",
-    c: "76",
-    answer: "B"
-  },
-  {
-    question: "What is 7 x 4?",
-    a: "21",
-    b: "27",
-    c: "28",
-    answer: "C"
-  },
-  {
-    question: "What is 16 / 4?",
-    a: "4",
-    b: "6",
-    c: "3",
-    answer: "A"
-  },
-  {
-    question: "What is 8 x 12?",
-    a: "88",
-    b: "112",
-    c: "96",
-    answer: "C"
-  }
-];
-// this get function is short for the getElementById function  
-function get(x) {
-  return document.getElementById(x);
-}
-// this function renders a question for display on the page
-function renderQuestion() {
-  test = get("test");
-  if (pos >= questions.length) {
-    test.innerHTML = "<h2>You got " + correct + " of " + questions.length + " questions correct</h2>";
-    get("test_status").innerHTML = "Test completed";
-    // resets the variable to allow users to restart the test
-    pos = 0;
-    correct = 0;
-    // stops rest of renderQuestion function running when test is completed
-    return false;
-  }
-  get("test_status").innerHTML = "Question " + (pos + 1) + " of " + questions.length;
+var question4 = {
+  question: "Which property is used to change the background color?",
+  answers: ["bgcolor", "color", "background-color"],
+  correct: 2
+};
 
-  question = questions[pos].question;
-  chA = questions[pos].a;
-  chB = questions[pos].b;
-  chC = questions[pos].c;
-  // display the question
-  test.innerHTML = "<h3>" + question + "</h3>";
-  // display the answer options
-  // the += appends to the data we started on the line above
-  test.innerHTML += "<label> <input type='radio' name='choices' value='A'> " + chA + "</label><br>";
-  test.innerHTML += "<label> <input type='radio' name='choices' value='B'> " + chB + "</label><br>";
-  test.innerHTML += "<label> <input type='radio' name='choices' value='C'> " + chC + "</label><br><br>";
-  test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
+var question5 = {
+  question: "Javasctipt is the same as Java?",
+  answers: ["True", "False"],
+  correct: 1
+};
+
+var question6 = {
+  question: "Which event occurs when the user clicks on an HTML element?",
+  answers: ["onchange", "onmouseclick", "onmouseover", "onclick"],
+  correct: 3
+};
+
+var question7 = {
+  question: "Is JavaScript case-sensitive?",
+  answers: ["True", "False"],
+  correct: 0
+};
+
+// create an array of objects
+
+var questions = [question1, question2, question3, question4, question5, question6, question7];
+
+// Initialize variables
+//------------------------------------------------------------------
+
+var tags;
+var tagsClass = '';
+var liTagsid = [];
+var correctAns = 0;
+var quizPage = 1;
+
+
+var currentIndex = 0;
+var currentQuestion = questions[currentIndex];
+
+var prevousQuestion;
+var previousIndex = 0;
+
+var ulTag = document.getElementsByTagName('ul')[0];
+var button = document.getElementById('submit');
+var questionTitle = document.getElementById('question');
+
+//save class name so it can be reused easily
+//if I want to change it, I have to change it one place
+var classHighlight = 'selected';
+
+
+// Display Answers and hightlight selected item
+//------------------------------------------------------------------
+function showQuestions() {
+
+  if (currentIndex != 0) {
+    // create again submit button only for next pages
+    ulTag.innerHTML = '';
+    button.innerHTML = 'Submit';
+    button.className = 'submit';
+    button.id = 'submit';
+
+    //update the number of questions displayed
+    document.getElementById('quizNumber').innerHTML = quizPage;
+  }
+
+  //Display Results in the final page
+  if (currentIndex == (questions.length)) {
+    ulTag.innerHTML = '';
+    document.getElementById('question').innerHTML = '';
+
+    showResults();
+
+    return
+  }
+
+  questionTitle.innerHTML = currentQuestion.question;
+  console.log(currentQuestion.question);
+
+  // create a for loop to generate the answers and display them in the page
+  for (var i = 0; i < currentQuestion.answers.length; i++) {
+    // creating answers
+    var newAns = document.createElement('li');
+    newAns.id = 'ans' + (i + 1);
+    newAns.className = "notSelected";
+    var textAns = document.createTextNode(currentQuestion.answers[i]);
+    newAns.appendChild(textAns);
+    var addNewAnsHere = document.getElementById('answer');
+    addNewAnsHere.appendChild(newAns);
+
+    console.log(currentQuestion.answers[i]);
+  }
+
+
+  //.click() will return the result of $('.notSelected')
+  var $liTags = $('.notSelected').click(function (list) {
+    list.preventDefault();
+    //run removeClass on every element
+    //if the elements are not static, you might want to rerun $('.notSelected')
+    //instead of the saved $litTags
+    $liTags.removeClass(classHighlight);
+    //add the class to the currently clicked element (this)
+    $(this).addClass(classHighlight);
+
+    //get id name of clicked answer
+    for (var i = 0; i < currentQuestion.answers.length; i++) {
+      // console.log(liTagsid[i]);
+      if ($liTags[i].className == "notSelected selected") {
+        //store information to check answer
+        tags = $liTags[i].id;
+        // tagsClass = $LiTags.className;
+        console.log(tags);
+        tagsClassName = $liTags[i];
+      }
+    }
+  });
+
+  //check answer once it has been submitted
+  button.onclick = function () { checkAnswer() };
 }
+
+//self calling function
+showQuestions();
+
+
+// Show Correct Answer
+//------------------------------------------------------------------
 function checkAnswer() {
-  // use getElementsByName because we have an array which it will loop through
-  choices = document.getElementsByName("choices");
-  for (var i = 0; i < choices.length; i++) {
-    if (choices[i].checked) {
-      choice = choices[i].value;
+  // get selected list
+  var selectedItem = document.getElementById(tags);
+
+  // check that an answer has been selected
+  if (selectedItem == undefined) {
+    alert("Please selected an answer!")
+    return
+  } else {
+    // get user answer if form of text
+    var userAns = selectedItem.innerHTML;
+  }
+
+  // change the background of the answer according to the Results
+  if (userAns == currentQuestion.answers[currentQuestion.correct]) {
+    console.log("Correct! The answer is: " + userAns);
+    // change color of selected item by changing className
+    selectedItem.className = 'correct';
+    // count the number of correct answers
+    correctAns++;
+    console.log(correctAns);
+  } else {
+    console.log("Wrong! The corrent answer is: " + currentQuestion.answers[currentQuestion.correct]);
+    //change the background of the wrong answer
+    selectedItem.className = 'wrong';
+    //hightlight the right answer if the user got it wrong
+    //change the class name of the correct answer
+    ulTag.getElementsByTagName('li')[currentQuestion.correct].className = 'correct';
+
+    console.log(currentQuestion.answers[currentQuestion.correct]);
+  }
+
+  // Create a next Question button once the answer has been submitted
+  button.innerHTML = 'Next Question';
+  button.className = 'next';
+  button.id = 'next';
+
+  prevousQuestion = currentQuestion;
+  quizPage++;
+  currentIndex++;
+  currentQuestion = questions[currentIndex];
+
+  // Start with the next question once the "Next" button has been clicked
+  button.onclick = function () { showQuestions() };
+  return
+}
+
+// Final score
+//------------------------------------------------------------------
+function showResults() {
+  //deleting page number
+  document.getElementById('pages').innerHTML = '';
+
+  // Change Title
+  questionTitle.innerHTML = '<h1>Your Score</h1>';
+
+  // Get the area that will be used to display the user's score
+  var newInfo = document.getElementById('quiz-results');
+  //Change the id and className of the area for the circle
+  newInfo.innerHTML = '';
+  newInfo.id = 'circle';
+  newInfo.className = 'circle';
+
+
+  //Create a Div for the fill element
+  var newDiv = document.createElement('div');
+  newDiv.className = 'fill';
+  var addHere = document.getElementById('circle');
+  addHere.appendChild(newDiv);
+
+  // add the score to the circle
+  var newScore = document.createElement('h3');
+  newScore.className = 'score';
+  var textScore = document.createTextNode(Math.floor((correctAns / questions.length) * 100) + '%');
+  newScore.appendChild(textScore);
+  addHere.appendChild(newScore);
+
+  //use jquary to grab the text of the score
+  var score = $(".score").text();
+
+  //fill the circle in base of the score
+  $(".fill").css("height", score);
+
+  if (correctAns >= 5) {
+    var newCongrats = document.createElement('p');
+    var textCongrats = document.createTextNode('Congratulations! You did a Good Job!')
+    newCongrats.appendChild(textCongrats);
+    document.getElementById('display-area').appendChild(newCongrats);
+
+    confettiEffect();
+  }
+
+}
+
+// Confetti Effect by Gtibo "Confetti Party"
+//------------------------------------------------------------------
+function confettiEffect() {
+  //grabing area to create the effect
+  canvas = document.getElementById("canvas");
+  context = canvas.getContext("2d");
+  width = canvas.width = window.innerWidth;
+  height = canvas.height = window.innerHeight;
+
+  // creating the tabel
+  particle = [];
+  particleCount = 0,
+    gravity = 0.3,
+    colors = [
+      '#f44336', '#e91e63', '#9c27b0', '#673ab7', '#3f51b5',
+      '#2196f3', '#03a9f4', '#00bcd4', '#009688', '#4CAF50',
+      '#8BC34A', '#CDDC39', '#FFEB3B', '#FFC107', '#FF9800',
+      '#FF5722', '#795548'
+    ];
+
+  for (var i = 0; i < 300; i++) {
+
+    particle.push({
+      x: width / 2,
+      y: height / 2,
+      boxW: randomRange(5, 20),
+      boxH: randomRange(5, 20),
+      size: randomRange(2, 8),
+
+      spikeran: randomRange(3, 5),
+
+      velX: randomRange(-8, 8),
+      velY: randomRange(-50, -10),
+
+      angle: convertToRadians(randomRange(0, 360)),
+      color: colors[Math.floor(Math.random() * colors.length)],
+      anglespin: randomRange(-0.2, 0.2),
+
+      draw: function () {
+        context.save();
+        context.translate(this.x, this.y);
+        context.rotate(this.angle);
+        context.fillStyle = this.color;
+        context.beginPath();
+
+        context.fillRect(this.boxW / 2 * -1, this.boxH / 2 * -1, this.boxW, this.boxH);
+        context.fill();
+        context.closePath();
+        context.restore();
+        this.angle += this.anglespin;
+        this.velY *= 0.999;
+        this.velY += 0.3;
+
+        this.x += this.velX;
+        this.y += this.velY;
+
+        if (this.y < 0) {
+          this.velY *= -0.2;
+          this.velX *= 0.9;
+        };
+
+        if (this.y > height) {
+          this.anglespin = 0;
+          this.y = height;
+          this.velY *= -0.2;
+          this.velX *= 0.9;
+        };
+
+        if (this.x > width || this.x < 0) {
+          this.velX *= -0.5;
+        };
+      },
+    });
+  }
+
+  function drawScreen() {
+    context.globalAlpha = 1;
+    for (var i = 0; i < particle.length; i++) {
+      particle[i].draw();
     }
   }
-  // checks if answer matches the correct choice
-  if (choice == questions[pos].answer) {
-    //each time there is a correct answer this value increases
-    correct++;
+
+  function loadImage(url) {
+    var img = document.createElement("img");
+    img.src = url;
+    return img;
   }
-  // changes position of which character user is on
-  pos++;
-  // then the renderQuestion function runs again to go to next question
-  renderQuestion();
+
+  function update() {
+    context.clearRect(0, 0, width, height);
+    drawScreen();
+    requestAnimationFrame(update);
+  }
+
+  update();
+
+  function randomRange(min, max) {
+    return min + Math.random() * (max - min);
+  }
+
+  function randomInt(min, max) {
+    return Math.floor(min + Math.random() * (max - min + 1));
+  }
+
+  function convertToRadians(degree) {
+    return degree * (Math.PI / 180);
+  }
+
+  function drawStar(cx, cy, spikes, outerRadius, innerRadius, color) {
+    var rot = Math.PI / 2 * 3;
+    var x = cx;
+    var y = cy;
+    var step = Math.PI / spikes;
+
+    context.strokeSyle = "#000";
+    context.beginPath();
+    context.moveTo(cx, cy - outerRadius)
+    for (i = 0; i < spikes; i++) {
+      x = cx + Math.cos(rot) * outerRadius;
+      y = cy + Math.sin(rot) * outerRadius;
+      context.lineTo(x, y)
+      rot += step
+
+      x = cx + Math.cos(rot) * innerRadius;
+      y = cy + Math.sin(rot) * innerRadius;
+      context.lineTo(x, y)
+      rot += step
+    }
+
+    context.lineTo(cx, cy - outerRadius)
+    context.closePath();
+    context.fillStyle = color;
+    context.fill();
+
+  }
 }
-// Add event listener to call renderQuestion on page load event
-window.addEventListener("load", renderQuestion);
+
 
 
 //-------- Pollution Map Code -------------//
